ec628794fddc13c3781885f48fb2a075cdabebb6
goju2k <goju2k@naver.com>
2025-03-08 11:24:21 +0900

    electron installed

---
+++ apps/client/file-mover-client/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'file-mover-client-react'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'client-file-mover-client';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
